d6b2c82a9147431dc79a917ffbeb7cee46041bea
Ryan Kunz <ryanmkunz@gmail.com>
2019-04-04 15:47:09 -0500

    End of day commit

--- battleShip/Ship.cs
+++ battleShip/Ship.cs
@@ -17,6 +17,16 @@ namespace battleShip
 
         }
 
+        public void MoveShip()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void PlaceShip()
+        {
+            throw new System.NotImplementedException();
+        }
+
         //TODO: write a parent class for all ships (destroyer 2, submarine 3, battleship 4, carrier 5)
 
         //methods
